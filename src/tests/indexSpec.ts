28e7ec79ef64f64471d0df58cabeefae50477b70
Shane Oston Stowe <31624311+shaneoston@users.noreply.github.com>
2022-02-24 19:47:13 -0700

    adds sharp and rns prettier and lint on project

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -4,7 +4,6 @@ import app from '../index'
 const request = supertest(app)
 
 describe('Test endpoint responses', () => {
-
     it('gets the endpoint response from the images route', async () => {
         const response = await request.get('/api/images')
         expect(response.status).toBe(200)
